e7096dc3ad199b59fc481e93792c2985a68043d3
Muhammad Tri Wibowo <akhmad.mib@gmail.com>
2024-09-30 03:31:56 +0700

    [CC-5] - feat add searching for compare keys and values

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 import { Buffer } from "buffer";
+import { ComparisonResult } from "@/entities/comparison_result";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -12,3 +13,25 @@ export const isBase64 = (value: string) => {
 
 export const base64Decode = (str: string):string => Buffer.from(str, 'base64').toString('binary');
 export const base64Encode = (str: string):string => Buffer.from(str, 'binary').toString('base64');
+
+export const filterComparisonResultList = (listResult: ComparisonResult[], searchTerm: string, searchCategory: string ): ComparisonResult[] => {
+  return listResult.filter(({ key, config1Value, config2Value }) => {
+      const lowerCaseSearchTerm = searchTerm.toLowerCase();
+  
+      if (searchCategory === 'All') {
+        return (
+            key.toLowerCase().includes(lowerCaseSearchTerm) ||
+            config1Value.toLowerCase().includes(lowerCaseSearchTerm) ||
+            config2Value.toLowerCase().includes(lowerCaseSearchTerm)
+        );
+      } else if (searchCategory === 'Key') {
+        return key.toLowerCase().includes(lowerCaseSearchTerm);
+      } else if (searchCategory === 'Config1Value') {
+        return config1Value.toLowerCase().includes(lowerCaseSearchTerm);
+      } else if (searchCategory === 'Config2Value') {
+        return config2Value.toLowerCase().includes(lowerCaseSearchTerm);
+      }
+  
+      return false;
+  });
+};
