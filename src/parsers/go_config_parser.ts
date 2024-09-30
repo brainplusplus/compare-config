12fd17baf47ae9f86c5db9443a99eb873bd92607
Muhammad Tri Wibowo <akhmad.mib@gmail.com>
2024-09-30 16:56:22 +0700

    hotfix: go parser Getenv from package os

--- src/parsers/go_config_parser.ts
+++ src/parsers/go_config_parser.ts
@@ -2,7 +2,7 @@
 export const parseGoConfig = (envString: string): { [key: string]: string } => {
     
     //const regex = /\b\w+\.(?:Get(?:String|Bool|Int|Duration|Int64|Int32|Float64|IntSlice|SizeInBytes|StringMap|StringMapString|StringSlice|StringMapStringSlice|Time|Uint|Uint16|Uint32|Uint64)|Getenv)\("([^"]+)"\)/g;
-    const regex = /\b(?:[a-zA-Z_]+\.(Get(?:String|Bool|Int|Duration|Int64|Int32|Float64|IntSlice|SizeInBytes|StringMap|StringMapString|StringSlice|StringMapStringSlice|Time|Uint|Uint16|Uint32|Uint64))|getEnv\w*)\(\s*"([^"]+)/g;
+    const regex = /\b(?:[a-zA-Z_]+\.(Get(?:String|Bool|Int|Duration|Int64|Int32|Float64|IntSlice|SizeInBytes|StringMap|StringMapString|StringSlice|StringMapStringSlice|Time|Uint|Uint16|Uint32|Uint64|env))|getEnv\w*)\(\s*"([^"]+)/g;
 
     const resultMap: Record<string, string> = {};
     let match;
